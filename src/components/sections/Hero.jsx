ff2042d5d00af7d3de083e172346291708e24082
krishna_9_99_999 <krishnakeshri9510ib@gmail.com>
2026-02-04 03:05:53 +0530

    update immage

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -149,7 +149,7 @@ const Hero = () => {
                 }}
               >
                 <img 
-                  src="https://drive.google.com/file/d/1CrQQs57sOm-zZX5zO-xWqOYl1ZU7NVOl/view?usp=drivesdk"
+                  src="https://ik.imagekit.io/y7ngbwzm5/my%20image"
                   alt="Krishna Keshri"
                   className="w-full h-full object-cover"
                 />
